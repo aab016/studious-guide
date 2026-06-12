5c11a274e6faa95f7bc32a9eef0630798837c6d7
aab016 <prof.alessandro_aglietti@iislagrange.edu.it>
2026-06-12 14:06:38 +0000

    fix(locomotion): try 2

--- iwsdk-demo/locomotion/src/panel.ts
+++ iwsdk-demo/locomotion/src/panel.ts
@@ -69,12 +69,14 @@ export class SettingsSystem extends createSystem({
         this.world.launchXR();
       });
 
+      /*
       try {
         this.world.launchXR();
         console.log("this.world.launchXR() success");
       } catch (error) {
         console.error('this.world.launchXR():', error);
       }
+      */
 
       const exitButton = document.getElementById('exit-button');
       exitButton.addEventListener('click', () => {
